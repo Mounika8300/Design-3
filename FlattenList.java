55d98b750eb7b0ba66b0346c3902d60ca64b36be
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-07-14 16:36:00 -0500

    Create FlattenList.java

---
+++ FlattenList.java
@@ -0,0 +1,28 @@
+public class NestedIterator implements Iterator<Integer> {
+   NestedInteger nextInt;
+   Stack<Iterator<NestedInteger>> stack;
+   public NestedIterator(List<NestedInteger> nestedList) {
+    stack = new Stack<Iterator<NestedInteger>>();
+    stack.push(nestedList.iterator());
+   }
+
+   @Override
+   public Integer next() {
+ return nextInt != null ? nextInt.getInteger() : null; 
+   }
+
+
+
+   @Override
+   public boolean hasNext() {
+       while (!stack.isEmpty()) {
+           if (!stack.peek().hasNext()) stack.pop();
+           else if ((nextInt = stack.peek().next()).isInteger()) return true;
+           else stack.push(nextInt.getList().iterator());
+       }
+
+
+
+       return false;
+   }
+}
